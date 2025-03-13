d42f30584b56f64fcb4c4d2d28ce154f75c6abcf
Encel Sanchez <encels@gmail.com>
2025-03-12 23:05:34 -0400

    Fix Types Doctrine

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,22 +1,53 @@
 <?php
 
+use App\Infrastructure\Persistence\Doctrine\Type\EmailType;
+use App\Infrastructure\Persistence\Doctrine\Type\NameType;
+use App\Infrastructure\Persistence\Doctrine\Type\PasswordType;
+use App\Infrastructure\Persistence\Doctrine\Type\UserIdType;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+if (!Type::hasType(UserIdType::NAME)) {
+    Type::addType(UserIdType::NAME, UserIdType::class);
+}
+
+if (!Type::hasType(NameType::NAME)) {
+    Type::addType(NameType::NAME, NameType::class);
+}
+
+if (!Type::hasType(EmailType::NAME)) {
+    Type::addType(EmailType::NAME, EmailType::class);
+}
+
+if (!Type::hasType(PasswordType::NAME)) {
+    Type::addType(PasswordType::NAME, PasswordType::class);
+}
+
+
+$paths = [__DIR__ . '/../src/Infrastructure/Persistence/Doctrine/Mapping'];
 $isDevMode = true;
 
 $dbParams = [
     'driver'   => 'pdo_mysql',
-    'user'     => 'user', 
+    'user'     => 'user',
     'password' => 'password',  
     'dbname'   => 'app_db',
     'host'     => 'mysql',
     'charset'  => 'utf8mb4'
 ];
 
+$config = ORMSetup::createXMLMetadataConfiguration($paths, $isDevMode);
 $connection = DriverManager::getConnection($dbParams, $config);
 $entityManager = new EntityManager($connection, $config);
+
+$platform = $connection->getDatabasePlatform();
+$platform->registerDoctrineTypeMapping('user_id', UserIdType::NAME);
+$platform->registerDoctrineTypeMapping('name', NameType::NAME);
+$platform->registerDoctrineTypeMapping('email', EmailType::NAME);
+$platform->registerDoctrineTypeMapping('password', PasswordType::NAME);
 
 return $entityManager;
